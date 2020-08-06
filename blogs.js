0f18e50dc618f0c611c89547b1f548c9e40480c4
mishel254 <sonimichie@gmail.com>
2020-08-06 15:59:52 +0300

    minor blog functionality changes

--- blogs.js
+++ blogs.js
@@ -1,6 +1,7 @@
 var ask = document.getElementById('ask');
 var display = document.getElementById('display');
 var query = document.getElementById('additions'); 
+var request = document.getElementById('request');
 
 // function to add a question 
 ask.addEventListener('click', (e) =>{
@@ -10,7 +11,7 @@ ask.addEventListener('click', (e) =>{
     var page = document.createElement('div');
     //selecting display and appending the created element
     document.getElementById('display').appendChild(page);
-    page.className = "section" ;
+    page.className = "section1" ;
     //creating and appending a paragraph to the el
     var paragraph = document.createElement('p');
     page.append(paragraph);
@@ -18,5 +19,25 @@ ask.addEventListener('click', (e) =>{
     paragraph.innerHTML = inputval;
     return inputval;
 });
+
+
+// function to add a question 
+request.addEventListener('click', (e) =>{
+    e.preventDefault();
+    var inputval = document.getElementById('textarea').value;   
+    //creating element
+    var page = document.createElement('div');
+    //selecting display and appending the created element
+    document.getElementById('display').appendChild(page);
+    page.className = "section1" ;
+    //creating and appending a paragraph to the el
+    var paragraph = document.createElement('p');
+    page.append(paragraph);
+    //fetching from input and adding data to the paragraph
+    paragraph.innerHTML = inputval;
+    return inputval;
+});
+
+
 
 
